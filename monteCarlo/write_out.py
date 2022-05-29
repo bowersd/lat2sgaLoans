c8d987af6612aec4b890dc17fba8c09dd18bb191
sautedman <bowersd@email.arizona.edu>
2022-05-29 14:23:38 -0700

    pt to cm, log scaling main squares

--- monteCarlo/write_out.py
+++ monteCarlo/write_out.py
@@ -37,19 +37,19 @@ def logblocks(*num_seqs):
     top = max([math.log(upto1(s[0]), 2) for s in num_seqs])
     right = sum([math.log(upto1(s[0]), 2) for s in num_seqs])
     boiler = ["\draw[xstep=0, ystep=1cm, opacity=0.1] (0pt,0pt) grid ({0}cm, 8cm);".format(right),
-        "\\foreach \y / \label in {50pt/50, 150pt/150, 250pt/250}",
-        "   {\draw (-18pt, \y) node {\huge \label} ;};",
+        "\\foreach \y / \label in {1cm/2, 2cm/4, 3cm/8, 4cm/16, 5cm/32, 6cm/64, 7cm/128, 8cm/256}\n",
+        "    {\draw (-18pt, \y) node {\huge \label} ;};\n"
             ]
     names = ["[p]$\\to$[k]",
-            "Len.",
-            "Harm.",
-            "Shorten.",
-            "Lengthen",
+            "Lenition",
+            "Harmony",
+            "Short.",
+            "Length",
             "Syncope",
             "Post-Syncope",
             ]
     for i in range(len(num_seqs)):
-        h.append("\draw({0}pt,0) rectangle ({1}pt, {2}pt);\n".format(edge, edge+num_seqs[i][0], num_seqs[i][0]))
+        h.append("\draw({0}cm,0) rectangle ({1}cm, {2}cm);\n".format(edge, edge+math.log(num_seqs[i][0],2), math.log(num_seqs[i][0],2)))
         h.append("\draw[fill=black!100]({0}pt,0) rectangle ({1}pt, {2}pt);\n".format((edge+(num_seqs[i][0]/2)-(num_seqs[i][1]/2)), (edge+(num_seqs[i][0]/2)+(num_seqs[i][1]/2)), num_seqs[i][1]))
         for x in s[2:]: h.append("\draw({0}pt,{1}pt) circle (1pt);\n".format((edge+(num_seqs[i][0]/2)), x))
         boiler.append("\draw ({0}, -36pt) node {\huge {1}};".format(edge+(s[0]/2), names[i]))
