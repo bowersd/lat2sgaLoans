90922fb003fe5611d2cb770ac32cce4b1cb9dc02
sautedman <bowersd@email.arizona.edu>
2022-05-29 21:33:21 -0700

    various debuggings. working log scale graphs now produced!

--- monteCarlo/write_out.py
+++ monteCarlo/write_out.py
@@ -6,10 +6,10 @@ def blocks(*num_seqs):
     top = max([s[0] for s in num_seqs])
     right = sum([s[0] for s in num_seqs])
     boiler = ["\draw[xstep=40pt, ystep=50pt, opacity=0.1] (0pt,0pt) grid (560pt, 250pt);\n",
-        "\\foreach \x / \label in {120pt/120, 240pt/240, 360pt/360, 480pt/480, 560pt/560}\n",
-        "    {\draw (\x, -10pt) node {\huge \label} ;};\n",
-        "\\foreach \y / \label in {50pt/50, 150pt/150, 250pt/250}\n",
-        "   {\draw (-18pt, \y) node {\huge \label} ;};\n",
+        r"\foreach \x / \label in {120pt/120, 240pt/240, 360pt/360, 480pt/480, 560pt/560}\n",
+        r"    {\draw (\x, -10pt) node {\huge \label} ;};\n",
+        r"\foreach \y / \label in {50pt/50, 150pt/150, 250pt/250}\n",
+        r"   {\draw (-18pt, \y) node {\huge \label} ;};\n",
             ]
     names = ["[p]$\\to$[k]",
             "Len.",
@@ -23,7 +23,7 @@ def blocks(*num_seqs):
         h.append("\draw({0}pt,0) rectangle ({1}pt, {2}pt);\n".format(edge, edge+num_seqs[i][0], num_seqs[i][0]))
         h.append("\draw[fill=black!100]({0}pt,0) rectangle ({1}pt, {2}pt);\n".format((edge+(num_seqs[i][0]/2)-(num_seqs[i][1]/2)), (edge+(num_seqs[i][0]/2)+(num_seqs[i][1]/2)), num_seqs[i][1]))
         for x in num_seqs[i][2:]: h.append("\draw({0}pt,{1}pt) circle (1pt);\n".format((edge+(num_seqs[i][0]/2)), x))
-        boiler.append("\draw ({0}, -36pt) node {\huge {1}};".format(edge+(num_seqs[i][0]/2), names[i]))
+        boiler.append("\draw ({0}, -36pt) node {{\huge {1}}};".format(edge+(num_seqs[i][0]/2), names[i]))
         edge += num_seqs[i][0]
     return boiler + h
 
@@ -31,12 +31,12 @@ def upto1(x):
     if x < 1: return 1
     return x
 
-def logblocks(*num_seqs):
+def logBlocks(*num_seqs):
     h = []
     edge = 0.0
     top = max([math.log(upto1(s[0]), 2) for s in num_seqs])
     right = sum([math.log(upto1(s[0]), 2) for s in num_seqs])
-    boiler = ["\draw[xstep=0, ystep=1cm, opacity=0.1] (0pt,0pt) grid ({0}cm, 8cm);".format(right),
+    boiler = ["\draw[xstep=0, ystep=1cm, opacity=0.1] (0,0) grid ({0}cm, 8cm);\n".format(right),
         "\\foreach \y / \label in {1cm/2, 2cm/4, 3cm/8, 4cm/16, 5cm/32, 6cm/64, 7cm/128, 8cm/256}\n",
         "    {\draw (-18pt, \y) node {\huge \label} ;};\n"
             ]
@@ -49,11 +49,11 @@ def logblocks(*num_seqs):
             "Post-Syncope",
             ]
     for i in range(len(num_seqs)):
-        h.append("\draw({0}cm,0) rectangle ({1}cm, {2}cm);\n".format(edge, edge+math.log(num_seqs[i][0],2), math.log(num_seqs[i][0],2)))
-        h.append("\draw[fill=black!100]({0}cm,0) rectangle ({1}cm, {2}cm);\n".format((edge+(math.log(num_seqs[i][0], 2)/2)-(math.log(num_seqs[i][1], 2)/2)), (edge+(math.log(num_seqs[i][0], 2)/2)+(math.log(num_seqs[i][1], 2)/2)), math.log(num_seqs[i][1], 2)))
-        for x in num_seqs[i][2:]: h.append("\draw({0}cm,{1}cm) circle (0.1cm);\n".format((edge+(math.log(num_seqs[i][0], 2)/2)), math.log(x, 2)))
-        boiler.append("\draw ({0}, -36pt) node {\huge {1}};".format(edge+(math.log(num_seqs[i][0], 2)/2), names[i]))
-        edge += num_seqs[i][0]
+        h.append("\draw({0}cm,0) rectangle ({1}cm, {2}cm);\n".format(edge, edge+math.log(upto1(num_seqs[i][0]),2), math.log(upto1(num_seqs[i][0]),2)))
+        h.append("\draw[fill=black!100]({0}cm,0) rectangle ({1}cm, {2}cm);\n".format((edge+(math.log(upto1(num_seqs[i][0]), 2)/2)-(math.log(upto1(num_seqs[i][1]), 2)/2)), (edge+(math.log(upto1(num_seqs[i][0]), 2)/2)+(math.log(upto1(num_seqs[i][1]), 2)/2)), math.log(upto1(num_seqs[i][1]), 2)))
+        for x in num_seqs[i][2:]: h.append("\draw({0}cm,{1}cm) circle (0.1cm);\n".format((edge+(math.log(upto1(num_seqs[i][0]), 2)/2)), math.log(upto1(x), 2)))
+        boiler.append("\draw ({0}, -36pt) node {{\huge {1}}};\n".format(edge+(math.log(upto1(num_seqs[i][0]), 2)/2), names[i]))
+        edge += math.log(upto1(num_seqs[i][0]), 2)
     return boiler + h
 
 def tikz(file_out, *lines):
@@ -70,4 +70,4 @@ def tikz(file_out, *lines):
         fout.write("\\end{document}"+"\n")
 
 if __name__ == "__main__":
-    for x in blocks((86.9,18), (80.6, 3), (78.9, 10), (61.5, 2), (50.8, 0), (52.9, 0), (120.4, 72)): print(x)
+    tikz('testfig.tex', *logBlocks(*((86.9,18), (80.6, 3), (78.9, 10), (61.5, 2), (50.8, 0), (52.9, 0), (120.4, 72))))
